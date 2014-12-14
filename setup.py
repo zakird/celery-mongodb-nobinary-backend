17937fb56f2f978c8215820b78b0da6935f5892e
Zakir Durumeric <zakir@ichnaea.eecs.umich.edu>
2014-12-13 19:24:11 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name = "celery-mongodb-nobinary-backend",
+    description = "Celery backend for MongoDB that does not convert task results into a binary format before storing them in Mongo database.",
+    version = "0.0.1",
+    license = "Apache License, Version 2.0",
+    author = "Zakir Durumeric",
+    author_email = "zakird@gmail.com",
+    maintainer = "Zakir Durumeric",
+    maintainer_email = "zakird@gmail.com",
+
+    keywords = "python celery mongodb backend",
+
+    packages = [
+        "zcelerybackend"
+    ],
+
+    install_requires=[
+        'setuptools',
+        'celery'
+    ]
+
+)
---
+++ zcelerybackend/mongodb.py
@@ -0,0 +1,42 @@
+"""
+Celery Backend for MongoDB that does not convert task results into a binary
+format before storing them in Mongo database.
+"""
+
+__copyright__ = "Copyright 2014 Regents of the University of Michigan"
+__author__ = "Zakir Durumeric"
+__license__ = "Apache License, Version 2.0"
+__email__ = "zakird@gmail.com"
+
+from celery.backends.mongodb import MongoBackend 
+
+__all__ = ['MongoNonBinaryBackend']
+
+
+class MongoNonBinaryBackend(MongoBackend):
+
+    def _store_result(self, task_id, result, status,
+                      traceback=None, request=None, **kwargs):
+        """Store return value and status of an executed task."""
+        meta = {'_id': task_id,
+                'status': status,
+                'result': result,
+                'traceback': Binary(self.encode(traceback)),
+                'children': Binary(self.encode(
+                    self.current_task_children(request),
+                ))}
+        # custom handling of data we embed in metadata
+        if "log" in result:
+            meta["log"] = result["log"]
+            del result["log"]
+        if "metadata" in result:
+            if "__name" in result["metadata"]:
+                meta["name"] = result["metadata"]["__name"]
+                del result["metadata"]["__name"]
+            meta["metadata"] = result["metadata"]
+            del result["metadata"]
+
+        self.collection.save(meta)
+
+        return result
+
