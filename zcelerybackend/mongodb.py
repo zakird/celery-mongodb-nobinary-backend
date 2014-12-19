c4d50134f4e2961b600079f30a672a25f880efc8
Zakir Durumeric <zakird@gmail.com>
2014-12-18 21:04:37 -0500

    handle logs and metadata when an exception is thrown

--- zcelerybackend/mongodb.py
+++ zcelerybackend/mongodb.py
@@ -16,27 +16,56 @@ __all__ = ['MongoNonBinaryBackend']
 class MongoNonBinaryBackend(MongoBackend):
 
     def _store_result(self, task_id, result, status,
-                      traceback=None, request=None, **kwargs):
+                      traceback, log, metadata, name,
+                      request=None, **kwargs):
         """Store return value and status of an executed task."""
         meta = {'_id': task_id,
                 'status': status,
                 'result': result,
+                'log': log,
+                'metadata': metadata,
                 'traceback': Binary(self.encode(traceback)),
                 'children': Binary(self.encode(
                     self.current_task_children(request),
                 ))}
         # custom handling of data we embed in metadata
-        if "log" in result:
-            meta["log"] = result["log"]
-            del result["log"]
-        if "metadata" in result:
-            if "__name" in result["metadata"]:
-                meta["name"] = result["metadata"]["__name"]
-                del result["metadata"]["__name"]
-            meta["metadata"] = result["metadata"]
-            del result["metadata"]
-
+        
         self.collection.save(meta)
 
+        return result
+
+    def store_result(self, task_id, result, status,
+                     traceback=None, request=None, **kwargs):
+        """Update task state and result."""
+
+	if "log" in result:
+            log = result["log"]
+            del result["log"]
+        elif hasattr(result, "log"):
+            log = getattr(result, "log")
+            delattr(result, "log")
+        else:
+            log = []
+
+        if "metadata" in result:
+            metadata = result["metadata"]
+            del result["metadata"]
+        elif hasattr(result, "metadata"):
+            metadata = getattr(result, "metadata")
+            delattr(result, "metadata")
+        else:
+            metadata  = {}
+
+        if "pretty_name" in metadata:
+            name = metadata["pretty_name"]
+            del metadata["pretty_name"]
+        else:
+            name = "unknown"
+
+        result = self.encode_result(result, status)
+
+        self._store_result(task_id, result, status, traceback,
+                           log, metadata, name,
+                           request=request, **kwargs)
         return result
 
